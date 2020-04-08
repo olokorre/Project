f4cba55f50c5b967d367883641f26d36bee22504
olokorre <andreiruansteffens@gmail.com>
2020-04-08 12:31:15 -0300

    Novo Objeto

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import pygame
 
 class Player(object):
-    def move(self):
-        pass
+    def __init__(self, img, largura, altura):
+        self.spritePlayer = pygame.image.load(img)
+        self.x =  (largura * 0.45)
+        self.y = (altura * 0.8)
+
+    def mover(self, tecla):
+        if keys[100]: self.x +=1 #letra D
+        if keys[97]: self.x -= 1 #letra A
+        if keys[119]: self.y -=1 #letra W
+        if keys[115]: self.y += 1 #letra S
+        gameDisplay.blit(self.spritePlayer, (self.x,self.y))
 
 pygame.init()
 
@@ -10,50 +19,35 @@ display_width = 800
 display_height = 600
 
 gameDisplay = pygame.display.set_mode((display_width,display_height))
-pygame.display.set_caption('Project: Cu!')
+pygame.display.set_caption('Project: Game!')
 
 black = (0,0,0)
 white = (255,255,255)
 
 clock = pygame.time.Clock()
 crashed = False
-carImg = pygame.image.load('assets/img/spritePlayer.png')
 
-print(type(carImg))
-
-def car(x,y):
-    gameDisplay.blit(carImg, (x,y))
+Jogador = Player("assets/img/spritePlayer.png", display_width, display_height)
 
 x =  (display_width * 0.45)
 y = (display_height * 0.8)
 
 while not crashed:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
+        if event.type == pygame.QUIT: crashed = True
 
+    if pygame.key.get_focused(): keys = pygame.key.get_pressed()
     gameDisplay.fill(white)
-    car(x,y)
+    Jogador.mover(keys)
 
         
     pygame.display.update()
     clock.tick(60)
 
-    keys = pygame.key.get_pressed()
-
-    cont = 0
-    for i in keys:
-        if i == 1: print(cont)
-        else: cont += 1
-
-    if keys[100]: #letra d
-        x +=1
-    if keys[97]: #letra a
-        x -= 1
-    if keys[119]: #letra w
-        y -=1
-    if keys[115]: #letra s
-        y += 1
+    # cont = 0
+    # for i in keys:
+    #     if i == 1: print(cont)
+    #     else: cont += 1
 
 pygame.quit()
 quit()
