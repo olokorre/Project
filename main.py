b92c129f47485753608c9ad830b6ccd1b9f3fdb0
olokorre <andreiruansteffens@gmail.com>
2020-04-11 22:13:14 -0300

    colisão

--- main.py
+++ main.py
@@ -8,13 +8,16 @@ class Player(object):
         self.x =  (largura * 0.45)
         self.y = (altura * 0.8)
 
-    def mover(self, tecla, x, y):
-        # if not self.hitBox.collidepoint(x, y):
+    def mover(self, tecla, bloco):
+        (x, y) = (self.x, self.y)
         if keys[275]: self.x += 3 #letra D
         elif keys[276]: self.x -= 3 #letra A
         if keys[273]: self.y -=3 #letra W
         elif keys[274]: self.y += 3 #letra S
         self.teleportar()
+        if self.hitBox.colliderect(bloco):
+            (self.x, self.y) = (x, y)
+            print ("Colisão!!!!!!!!")
         gameDisplay.blit(self.spritePlayer, (self.x,self.y))
     
     def teleportar(self):
@@ -24,12 +27,22 @@ class Player(object):
         elif self.y <= 0: self.y = 600
 
 
-class Wall(object):
+class Ground(object):
     def __init__(self, img):
         self.spriteWall = pygame.image.load(img)
 
     def montar(self, x, y):
-        gameDisplay.blit(self.spriteWall, (x ,y))
+        gameDisplay.blit(self.spriteWall, (x ,y))    
+
+class Wall(Ground):
+    def __init__(self, img, x, y):
+        self.x = x
+        self.y = y
+        self.spriteWall = pygame.image.load(img)
+        self.hitBox = pygame.Rect(32, 32, 32, 32)
+
+    def montar(self):
+        gameDisplay.blit(self.spriteWall, (self.x ,self.y))   
 
 pygame.init()
 
@@ -46,15 +59,11 @@ clock = pygame.time.Clock()
 crashed = False
 
 Jogador = Player("assets/img/spritePlayer.png", display_width, display_height)
-Chao = Wall("assets/img/T6.png")
-Parede = Wall("assets/img/P6.png")
+Chao = Ground("assets/img/T6.png")
+Parede = Wall("assets/img/P6.png", random.randint(0, 800), random.randint(0, 600))
 
 x =  (display_width * 0.45)
 y = (display_height * 0.8)
-
-paredeX = random.randint(0, 800)
-paredeY = random.randint(0, 600)
-
 
 while not crashed:
     for event in pygame.event.get():
@@ -67,9 +76,9 @@ while not crashed:
         for l in range(0, 600, 64):
             Chao.montar(i ,l)
     
-    Jogador.mover(keys, paredeX, paredeY)
-    Parede.montar(paredeX, paredeY)
-        
+    Jogador.mover(keys, Parede.hitBox)
+    Parede.montar()
+ 
     pygame.display.update()
     clock.tick(60)
 
